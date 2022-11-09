5d559441024cd2b42740fbccfcc6a5e14bd22f8b
Chris <christopher.sarm15@gmail.com>
2022-11-08 23:37:24 -0500

    updated typedefs and resolvers for user login using jsonwebtoken and created auth file in utils

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,28 +1,48 @@
 const { User, Post } = require("../models");
-
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        //get all posts by user or all posts 
-        posts: async (parent, { username }) => {
-            const params = username ? { username } : {};
-            return Post.find().sort({ createdAt: -1 });
-        },
-        // get post by id
-        post: async (parent, { _id }) => {
-            return Post.findOne({ _id });
-        },
-        //get all users
-        users: async() => {
-            return User.find()
-                .select("-__v -password")
-                .populate("posts");
-        },
-        // get user by username
-        user: async(parent, { username }) => {
-            return User.findOne({ username })
-                .select("-__v -password")
-                .populate("posts");
-        }
-    }
-}
+  Query: {
+    //get all posts by user or all posts
+    posts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Post.find().sort({ createdAt: -1 });
+    },
+    // get post by id
+    post: async (parent, { _id }) => {
+      return Post.findOne({ _id });
+    },
+    //get all users
+    users: async () => {
+      return User.find().select("-__v -password").populate("posts");
+    },
+    // get user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("posts");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
