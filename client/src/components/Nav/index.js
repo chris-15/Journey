7b84449d4a87a2d2af07399a9078ee29759ac353
Chris <christopher.sarm15@gmail.com>
2023-03-30 17:44:07 -0400

    started adding components folder and nav component, not much functionality in it though- soon to come

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+
+    // create function here to show Nav bar if user is logged in
+
+    return(
+        <header>
+            <h1>Cancer Blog</h1>
+
+        </header>
+    )
+}
+
+export default Nav;
