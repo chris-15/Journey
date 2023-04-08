4176fe34034bd909804005b4c191fe6f0dec9c7b
Chris <christopher.sarm15@gmail.com>
2023-04-07 23:50:59 -0400

    installed tailwind to use for css

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,17 +1,17 @@
 const Nav = () => {
+  // create function here to show Nav bar if user is logged in
 
-    // create function here to show Nav bar if user is logged in
-
-    return(
-        <header>
-            <h1>Cancer Blog</h1>
-            <ul>
-                <li>Login</li>
-                <li>SignUp</li>
-            </ul>
-
-        </header>
-    )
-}
+  return (
+    <header>
+      <h1>Cancer Blog</h1>
+      <nav>
+        <ul>
+          <li>Login</li>
+          <li>SignUp</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Nav;
