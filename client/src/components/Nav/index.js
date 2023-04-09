d18a02a0e77606f28e2478daadcaea245b0a0f9b
Chris <christopher.sarm15@gmail.com>
2023-04-09 16:43:37 -0400

    added some basic css

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,14 +2,14 @@ const Nav = () => {
   // create function here to show Nav bar if user is logged in
 
   return (
-    <header>
-      <h1>Cancer Blog</h1>
-      <nav>
-        <ul>
-          <li>Login</li>
-          <li>SignUp</li>
-        </ul>
-      </nav>
+    <header className="flex justify-between">
+        <h1>Cancer Blog</h1>
+        <nav>
+          <ul className="flex">
+            <li>Login</li>
+            <li>SignUp</li>
+          </ul>
+        </nav>
     </header>
   );
 };
