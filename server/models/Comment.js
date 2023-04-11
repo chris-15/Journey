b878ae11d375ae94c4ba0797fc8aa249d60013af
Chris <christopher.sarm15@gmail.com>
2023-04-10 22:07:31 -0400

    added date format functin to comments model in backend server

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,4 +1,5 @@
 const { Schema } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const commentSchema = new Schema (
     {
@@ -14,7 +15,7 @@ const commentSchema = new Schema (
         createdAt: {
             type: Date,
             default: Date.now,
-            // add util function for date
+            get: timestamp => dateFormat(timestamp)
         }
     },
     {
