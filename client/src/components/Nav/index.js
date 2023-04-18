445561a08f09dfbf117fb8d6106a2d81eb77cdb1
Chris <christopher.sarm15@gmail.com>
2023-04-17 21:56:29 -0400

    adding some basic styling to nav bar component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth'
+
 const Nav = () => {
   // create function here to show Nav bar if user is logged in
 
   return (
-    <header className="flex justify-between">
+    <header className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-red-500 text-white">
         <h1>Cancer Blog</h1>
         <nav>
-          <ul className="flex">
-            <li>Login</li>
-            <li>SignUp</li>
-          </ul>
+          <a href='/' className="mr-4">Log in</a>
+          <a href='/'>Sign Up</a>
+          
         </nav>
     </header>
   );
