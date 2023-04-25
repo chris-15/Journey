b28fc4e241dcf7a065dda0d27661d2b52d9c9349
Chris <christopher.sarm15@gmail.com>
2023-04-24 21:46:39 -0400

    started implementing login form handle functions

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,44 +1,83 @@
-import { LOGIN_USER } from '../utils/mutations';
-import { useMutation } from '@apollo/client';  
-import Auth from '../utils/auth';
-
+import { LOGIN_USER } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
 
 const Login = () => {
+  const [login, { error }] = useMutation(LOGIN_USER);
+  const [formState, setFormState] = useState({ email: "", password: "" });
 
-    const [login, { error }] = useMutation(LOGIN_USER);
-    const [formState, setFormState] = useState({ email: '', password: ''});
+  // on change function anytime input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (event) {
+      console.error(event);
+    }
+
+    //clears the form values
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
 
   return (
     <main>
-        <div>
-            <form>
-                <h2>Login</h2>
-               <div>
-               <label className='' htmlFor='email'>
-                    Email:
-                </label>
-                <input className='' id='email' type='email' name='email'/>
-               </div>
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <h2>Login</h2>
+          <div>
+            <label className="" htmlFor="email">
+              Email:
+            </label>
+            <input
+              className=""
+              id="email"
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+          </div>
 
-               <div>
-                <label className=''>
-                    Password:
-                </label>
-                <input className='' id='password' type='password' name='password'/>
-               </div>
+          <div>
+            <label className="">Password:</label>
+            <input
+              className=""
+              id="password"
+              type="password"
+              name="password"
+              value={formState.email}
+              onChange={handleChange}
+            />
+          </div>
 
-               <div>
-                <button className='' type='submit'>
-                    Log In
-                </button>
-               </div>
-            </form>
-        </div>
+          <div>
+            <button className="" type="submit">
+              Log In
+            </button>
+          </div>
+        </form>
+
+        {error && <div>Incorrect credentials</div>}
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
