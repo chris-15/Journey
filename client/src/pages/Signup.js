5e082f9fdbb1a98427e838b3d556a15bf1f034c3
Chris <christopher.sarm15@gmail.com>
2023-04-26 14:30:11 -0400

    added initial styling to sign up page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+
 
 const Signup = () => {
-    const [formState, setFormState] = useState({
-        username: '',
-        email: '',
-        password: '',
-    });
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-    const [addUser, { error }] = useMutation(ADD_USER);
+  const [addUser, { error }] = useMutation(ADD_USER);
 
-    // on change function anytime input changes
+  // on change function anytime input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
 
@@ -36,41 +38,66 @@ const Signup = () => {
   };
 
   return (
-    <main>
-        <div>
-            <form>
-                <h2>
-                    Create Your Account
-                </h2>
-                {/* username */}
-                <div>
-                    <label htmlFor='username'>Username</label>
-                    <input id='username' type='username' name='username'></input>
-                </div>
+    <main className="grid grid-cols-1 h-screen w-full">
+      <div className="flex flex-col justify-center">
+        <form
+          className="max-w-[400px] w-full mx-auto"
+          onSubmit={handleFormSubmit}
+        >
+          <h2 className="text-4xl font-bold text-center py-6">
+            Create Your Account
+          </h2>
+          {/* username */}
+          <div className="flex flex-col py-2">
+            <label htmlFor="username">Username</label>
+            <input
+              className="border-2 p-2"
+              id="username"
+              type="username"
+              name="username"
+              value={formState.username}
+              onChange={handleChange}
+            />
+          </div>
 
-                {/* email */}
-                <div>
-                    <label htmlFor='email'>Email</label> 
-                    <input id='email' type='email' name='email'></input> 
-                </div>
+          {/* email */}
+          <div className="flex flex-col py-2">
+            <label htmlFor="email">Email</label>
+            <input
+              className="border-2 p-2"
+              id="email"
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+            />
+          </div>
 
-                {/* password */}
-                <div>
-                    <label htmlFor='password'>Password</label> 
-                    <input id='password' type='password' name='password'></input> 
-                </div>
+          {/* password */}
+          <div className="flex flex-col py-2">
+            <label htmlFor="password">Password</label>
+            <input
+              className="border-2 p-2"
+              id="password"
+              type="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+            />
+          </div>
 
-                <div>
-                    <button type='submit'>CREATE ACCOUNT</button>
-                </div>
+          <div>
+            <button className="border w-full my-5 py-2 bg-[#FF0022] text-white hover:underline" type="submit">CREATE ACCOUNT</button>
+          </div>
 
-                <div>
-                    <p>Already have an account? Log in</p>
-                </div>
-            </form>
-        </div>
+          <div className="flex justify-around">
+            <p>Already have an account? <span className="font-bold hover:text-[#FF0022] hover:underline">Log in</span></p>
+          </div>
+        </form>
+        {error && <div>Something went wrong. Please try again!</div>}
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
