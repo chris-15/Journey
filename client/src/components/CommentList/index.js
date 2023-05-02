5784447a7d23850446d699c82337fc368b577dbf
Chris <christopher.sarm15@gmail.com>
2023-05-01 22:50:40 -0400

    added initial comment list component

---
+++ client/src/components/CommentList/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const CommentList = ({ comments }) => {
+  return (
+    <div>
+      <div>
+        <h4>Comments</h4>
+      </div>
+      <div>
+        {comments &&
+          comments.map((comment) => (
+            <div>
+              <p>
+                {comment.username} on {comment.createdAt}
+              </p>
+              <p key={comment._id}>{comment.commmentText}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_POST } from "../utils/queries";
 
+import { CommentList } from "../components/CommentList";
+
 const SinglePost = () => {
   const { id: postId } = useParams();
 
@@ -18,17 +20,19 @@ const SinglePost = () => {
   }
 
   return (
-    <main className="grid grid-cols-1 h-screen w-full">
+    <main className="grid grid-cols-1 w-full">
       <div className="max-w-[400px] sm:max-w-[600px] md:max-w-[800px] w-full mx-auto py-6">
-          <h2 className="text-2xl text-center">{post.postTitle}</h2>
-        
-        <div className="border-4 border-solid border-black p-2 my-2">
+        <h2 className="text-2xl text-center">{post.postTitle}</h2>
+
+        <div className="border-2 border-solid border-black p-2 my-2">
           <p>
             {post.username} on {post.createdAt}
           </p>
-          <p>{post.postText}</p>
+          <p className="">{post.postText}</p>
         </div>
-          
+      </div>
+      <div className="">
+      <CommentList comments={post.comments}/>
       </div>
     </main>
   );
