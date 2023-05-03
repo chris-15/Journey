429c5b0e7f1e0adfcdb0e0399ae658641cc44d7c
Chris <christopher.sarm15@gmail.com>
2023-05-02 22:36:28 -0400

    added comment form functionality

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -4,12 +4,46 @@ import { useMutation } from "@apollo/client";
 import { ADD_COMMENT } from "../../utils/mutations";
 
 const CommentForm = ({ postId }) => {
-  return (
+  const [commentText, setCommentText] = useState("");
+  const [characterCount, setCharacterCount] = useState(0);
+  const [addComment, { error }] = useMutation(ADD_COMMENT);
 
+  // function to handle any change from input
+  const handleChange = (event) => {
+    if (event.target.value.length <= 500) {
+      setCommentText(event.target.value);
+      setCharacterCount(event.target.value.length);
+      console.log(commentText)
+    }
+  };
+
+  // function to handle form submit
+  const handleFormSubmit = async (event) => {
+    try {
+      await addComment({
+        variables: { commentText, postId },
+      });
+      // this clears the form value
+      setCommentText("");
+      setCharacterCount(0);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
     <div>
-      <form>
+        <p>
+            Character Count: {characterCount}/500
+            {error && <span>Something went wrong!</span>}
+        </p>
+      <form onSubmit={handleFormSubmit}>
         <div>
-          <textarea placeholder="Add a comment..."></textarea>
+          <textarea
+            placeholder="Add a comment..."
+            value={commentText}
+            onChange={handleChange}
+          ></textarea>
           <button>submit</button>
         </div>
       </form>
