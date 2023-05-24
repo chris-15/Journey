83356884d81fc75aaa2d0171f398b0bc5a0691a6
Chris <christopher.sarm15@gmail.com>
2023-05-23 23:12:50 -0400

    removed some comments

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
 
 const Nav = () => {
-  // create function here to show Nav bar if user is logged in
+
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
 
-  // need to change a tags to use link tags and set up routes using react router dom
+  
 
   return (
     <header className="w-full h-[80px] flex justify-between items-center px-4 bg-[#FF0022] text-white shadow-xl">
