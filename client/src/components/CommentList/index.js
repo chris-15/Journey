1efcbc95ab2bb5584eddfb83e4f25eeb950ccc3f
Chris <christopher.sarm15@gmail.com>
2023-05-29 17:24:18 -0400

    added more styling to home page, added links in comments for username

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from 'react-router-dom' 
 
 const CommentList = ({ comments }) => {
-// reversing the comments array so the latest comments get displayed
-// slice creates a new array so the original comment array goes unchanged
-  const reversedCommentsArr = comments.slice().reverse()
+  // reversing the comments array so the latest comments get displayed
+  // slice creates a new array so the original comment array goes unchanged
+  const reversedCommentsArr = comments.slice().reverse();
 
   return (
     <div className="max-w-[400px] sm:max-w-[600px] md:max-w-[800px] w-full mx-auto py-6">
@@ -13,13 +14,16 @@ const CommentList = ({ comments }) => {
       <div className="">
         {reversedCommentsArr &&
           reversedCommentsArr.map((comment) => (
-            <div className="mb-4 pb-2 border-b-2 border-dotted" key={comment._id}>
-               <p className="mb-1 text-lg">{comment.commentText}</p>
+            <div
+              className="mb-4 pb-2 border-b-2 border-dotted"
+              key={comment._id}
+            >
+              <p className="mb-1 text-lg">{comment.commentText}</p>
 
               <p className="text-sm">
-                {comment.username} on <span className="text-gray-600">{comment.createdAt}</span> 
+                <span className="font-extrabold hover:text-[#FF0022]"><Link to={`/profile/${comment.username}`}>{comment.username}</Link></span>{' '}
+                <span className="text-gray-600">{comment.createdAt}</span>
               </p>
-              
             </div>
           ))}
       </div>
--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -12,35 +12,34 @@ const PostList = ({ posts, title }) => {
   return (
 
     <div className=" py-12">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-3xl font-bold text-center mb-8">{title}</h2>
-        <div className="grid gap-8 sm:grid-cols-2">
-          {posts &&
-            posts.map((post) => (
-              <div
-                key={post._id}
-                className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
-              >
-                
-                  <div className="p-6">
-                    <h3 className="text-xl font-semibold mb-4 hover:text-[#FF0022] hover:underline"><Link to={`/post/${post._id}`}>{post.postTitle}</Link></h3>
-                    <div className="flex items-center text-sm text-gray-600">
-                      <p className="mr-2">
-                        <span className="text-black font-extrabold hover:text-[#FF0022]"><Link to={`/profile/${post.username}`}>{post.username}</Link></span> posted on{' '}
-                        {post.createdAt}
-                      </p>
-                      <p className="border-l-2 pl-2 ml-2 text-gray-500">
-                        Total Comments: {post.commentCount}
-                      </p>
-                    </div>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <h2 className="text-3xl font-bold text-center mb-8">{title}</h2>
+      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        {posts &&
+          posts.map((post) => (
+            <div
+              key={post._id}
+              className="bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+            >
+              
+                <div className="p-6">
+                  <h3 className="text-xl font-semibold mb-4 hover:text-[#FF0022] hover:underline"><Link to={`/post/${post._id}`}>{post.postTitle}</Link></h3>
+                  <div className="flex items-center text-sm text-gray-600">
+                    <p className="mr-2">
+                      <span className="text-black font-extrabold hover:text-[#FF0022]"><Link to={`/profile/${post.username}`}>{post.username}</Link></span> posted on{' '}
+                      {post.createdAt}
+                    </p>
+                    <p className="border-l-2 pl-2 ml-2 text-gray-500">
+                      Total Comments: {post.commentCount}
+                    </p>
                   </div>
-                
-              </div>
-            ))}
-        </div>
+                </div>
+              
+            </div>
+          ))}
       </div>
     </div>
-
+  </div>
     
   );
 };
