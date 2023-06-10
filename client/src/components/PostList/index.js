2b86dae7625dae56e56de3bae2cd33c9c43e518e
Chris <christopher.sarm15@gmail.com>
2023-06-09 22:11:01 -0400

    added a preview of the blog post on the post list component

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { FaComment } from "react-icons/fa"
+import { FaComment } from "react-icons/fa";
 
 const PostList = ({ posts, title }) => {
   if (!posts || !posts.length) {
@@ -14,7 +14,7 @@ const PostList = ({ posts, title }) => {
     <div className="py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-bold text-center mb-8">{title}</h2>
-        <div className="grid gap-8 sm:grid-cols-2">
+        <div className="grid gap-8 md:grid-cols-2">
           {posts &&
             posts.map((post) => (
               <div
@@ -26,6 +26,10 @@ const PostList = ({ posts, title }) => {
                     <Link to={`/post/${post._id}`}>{post.postTitle}</Link>
                   </h3>
 
+                  <p className="mb-4 text-gray-500 line-clamp-2">
+                    {post.postText}
+                  </p>
+
                   <div className="flex items-center text-sm text-gray-600">
                     <p className="mr-2">
                       <span className="text-black font-extrabold hover:text-[#40c3c2]">
@@ -36,13 +40,16 @@ const PostList = ({ posts, title }) => {
                       posted on {post.createdAt}
                     </p>
                   </div>
-            
+
                   <div className="flex justify-between items-center border-t-2 my-2 pt-2">
                     <h3 className="rounded-xl py-1 px-2.5 bg-gradient-to-r from-[#40c3c2] to-[#0081a7] text-md text-white">
                       #{post.category}
                     </h3>
                     <p className="text-md text-gray-500 flex items-center">
-                      <span className="mr-2"><FaComment size={20}></FaComment></span>{post.commentCount}
+                      <span className="mr-2">
+                        <FaComment size={20}></FaComment>
+                      </span>
+                      {post.commentCount}
                     </p>
                   </div>
                 </div>
